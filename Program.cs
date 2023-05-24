ed80330db8593f970ba578b738a5764fb12aac59
j.pikin <pikin@bk.ru>
2023-05-24 13:05:40 +0500

    Task 68 added

--- Program.cs
+++ Program.cs
@@ -10,13 +10,38 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-int M = GetNum("Введите число M");
-int N = GetNum("Введите число N");
-int sum = 0;
+// int M = GetNum("Введите число M");
+// int N = GetNum("Введите число N");
+// int sum = 0;
 
-if (M < N) GetSum(M, N);
-else GetSum(N, M);
-Console.WriteLine($"Сумма Натуральных чисел в промежутке от {M} до {N} составит {sum}");
+// if (M < N) GetSum(M, N);
+// else GetSum(N, M);
+// Console.WriteLine($"Сумма Натуральных чисел в промежутке от {M} до {N} составит {sum}");
+
+// int GetNum(string str)
+// {
+//     System.Console.Write(str + " ");
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// void GetSum(int num1, int num2)
+// {
+//     if (num1 > num2) return;
+
+//     sum += num1;
+//     GetSum(num1 + 1, num2);
+// }
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int m = GetNum("Введите число m");
+int n = GetNum("Введите число n");
+
+Console.WriteLine($"Функция Аккермана для чисел A({m},{n}) = {Ackermann(m, n)}");
 
 int GetNum(string str)
 {
@@ -24,16 +49,10 @@ int GetNum(string str)
     return Convert.ToInt32(Console.ReadLine());
 }
 
-void GetSum(int m, int n)
+int Ackermann(int numM, int numN)
 {
-    if (m > n) return;
-
-    sum += m;
-    GetSum(m + 1, n);
+    if (numM == 0) return numN + 1;
+    if (numM != 0 && numN == 0) return Ackermann(numM - 1, 1);
+    if (numM > 0 && numN > 0) return Ackermann(numM - 1, Ackermann(numM, numN - 1));
+return Ackermann(m, n);
 }
-
-
-// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
-// Даны два неотрицательных числа m и n.
-// m = 2, n = 3 -> A(m,n) = 9
-// m = 3, n = 2 -> A(m,n) = 29
